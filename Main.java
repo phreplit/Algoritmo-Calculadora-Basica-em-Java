1478acf36f0869bc4f4c8aeb2ec3703065aab635
Paulo Henrique <147956669+phreplit@users.noreply.github.com>
2024-11-12 15:57:06 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -134,8 +134,8 @@ public class Main {
       }            
     }
    }
-      public static void clear() {
-	         System.out.print("\033[H\033[2J"); // clear screen
-	         System.out.flush();
-      }
+   public static void clear() {
+	 System.out.print("\033[H\033[2J"); // clear screen
+	 System.out.flush();
+   }
   }
